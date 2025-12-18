022a621b5b9028f1187f81be2f66275fbfca10ef
Juan Tardivo <juan.tardivo@diveria.com>
2025-12-18 17:15:14 -0300

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Esto es crucial para static export
+  basePath: '/Inmobiliaria', // Reemplaza con el nombre exacto de tu repo
+  assetPrefix: '/Inmobiliaria/',
+  images: {
+    unoptimized: true, // Necesario para static export
+  },
 };
 
 export default nextConfig;
