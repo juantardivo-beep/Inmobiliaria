c25e6e8c031b7cc57749134058b0863444decbdf
Juan Tardivo <juan.tardivo@diveria.com>
2025-12-24 21:15:13 -0300

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   output: 'export',
-//   basePath: '/Inmobiliaria',
-//   assetPrefix: '/Inmobiliaria/',
-//   images: {
-//     unoptimized: true,
-//   },
-// };
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/Inmobiliaria',
+  assetPrefix: '/Inmobiliaria/',
+  images: {
+    unoptimized: true,
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
