4ff895d6797865313a80f142637b6e6a250b165d
Juan Tardivo <juan.tardivo@diveria.com>
2025-12-30 12:59:23 -0300

    feat: all views implemented and wpp icon

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -1,11 +1,11 @@
 "use client"
 import Carrousel from "../../components/molecules/carrousel/Carrousel"
 import AboutUS from "../../components/organisms/aboutUS/aboutUS"
-import Inmobiliaria1 from '../../../assets/inmobiliaria_1.jpg'
-import Inmobiliaria2 from '../../../assets/inmobiliaria_2.jpg'
-import Inmobiliaria3 from '../../../assets/inmobiliaria_3.jpg'
-import Inmobiliaria4 from '../../../assets/inmobiliaria_4.jpg'
-import Inmobiliaria5 from '../../../assets/inmobiliaria_5.jpg'
+import Inmobiliaria1 from '../../assets/inmobiliaria_1.jpg'
+import Inmobiliaria2 from '../../assets/inmobiliaria_2.jpg'
+import Inmobiliaria3 from '../../assets/inmobiliaria_3.jpg'
+import Inmobiliaria4 from '../../assets/inmobiliaria_4.jpg'
+import Inmobiliaria5 from '../../assets/inmobiliaria_5.jpg'
 
 export default function NosotrosPage() {
 
